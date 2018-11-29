5fe2e3819fdfdad76e1f1d74ae6a2a1136b0fa2a
Suweon Shin <suweon.shin@linecorp.com>
2018-11-29 17:09:26 +0900

    fix mutex scope

--- example/TestConcur.cpp
+++ example/TestConcur.cpp
@@ -15,7 +15,7 @@ bool CTestConcur::StartTest(const std::string &strHost, int port)
 
 
 	//if (!m_redis.Initialize(strHost, port, 3, 3, 20))
-	if (!m_redis.Initialize(strHost, port, 3, 3, 2))
+	if (!m_redis.Initialize(strHost, port, 3, 3, 20))
 	{
 		log_error("Connect to redis failed [ip:", strHost, "][port:", port, "]");
 		return false;
@@ -28,10 +28,10 @@ bool CTestConcur::StartTest(const std::string &strHost, int port)
 	}
 
 	m_bExit = false;
-	//const int nGetTrdNum = 20;
-	//const int nSetTrdNum = 20;
-	const int nGetTrdNum = 1;
-	const int nSetTrdNum = 1;
+	const int nGetTrdNum = 20;
+	const int nSetTrdNum = 20;
+	//const int nGetTrdNum = 1;
+	//const int nSetTrdNum = 1;
 	std::thread *pthreadGet[nGetTrdNum] = {nullptr};
 	std::thread *pthreadSet[nSetTrdNum] = { nullptr };
 	for (int i = 0; i < nGetTrdNum; ++i)
@@ -117,7 +117,7 @@ void CTestConcur::Test_Get()
 			m_mutex.unlock();
 			//m_bExit = true;
 		}
-		std::this_thread::sleep_for(std::chrono::milliseconds(100));
+		std::this_thread::sleep_for(std::chrono::milliseconds(1000));
 		if (nIndex > 10)
 			nIndex = 1;
 	}
@@ -132,42 +132,42 @@ void CTestConcur::Test_Set()
 	//struct timezone tz;
 	while (!m_bExit)
 	{
-		m_mutex.lock();
+		//m_mutex.lock();
 		//std::chrono::time_point<std::chrono::system_clock> start_time(std::chrono::system_clock::now());
 		auto start_time = std::chrono::system_clock::now();
 		std::stringstream ssKey, ssVal;
 		ssKey << "tk_str_" << nIndex++;
 		ssVal << "value_" << nCounter++;
 
-		auto connection = m_redis.AttachConnection(m_redis.HASH_SLOT(ssKey.str()));
-		if (nullptr == connection)
-		{
-			log_error("connection is null");
-			return;
-		}
+		//auto connection = m_redis.AttachConnection(m_redis.HASH_SLOT(ssKey.str()));
+		//if (nullptr == connection)
+		//{
+		//	log_error("connection is null");
+		//	return;
+		//}
 		//m_redis.Watch(connection, ssKey.str());
 
-		//int nRet = m_redis.Set(ssKey.str(), ssVal.str());
-		int nRet = m_redis.Set(connection, ssKey.str(), ssVal.str());
+		int nRet = m_redis.Set(ssKey.str(), ssVal.str());
+		//int nRet = m_redis.Set(connection, ssKey.str(), ssVal.str());
 		//m_redis.Exec(connection, ssKey.str());
 		if (nRet == RC_SUCCESS)
 		{
-			//m_mutex.lock();
+			m_mutex.lock();
 			if (++npc > NUM_DEF)
 			{
 				auto end_time = std::chrono::system_clock::now();
-				std::chrono::duration<double, std::ratio<1, 1000>> diff;
+				std::chrono::duration<double, std::ratio<1, 10>> diff;
 				diff = end_time - start_time;
 				//std::chrono::time_point<std::chrono::system_clock> end_time(std::chrono::system_clock::now());
 				log_debug("Set OK [key:", ssKey.str(), "][value:", ssVal.str(), "][Time:", diff.count(), "ms]");
 				npc = 0;
 			}
-			//m_mutex.unlock();
+			m_mutex.unlock();
 		}
 		else
 		{
 			//gettimeofday(&tv, &tz);
-			//m_mutex.lock();
+			m_mutex.lock();
 			if (++npc > NUM_DEF)
 			{
 				if (nRet == RC_NO_RESOURCE)
@@ -182,13 +182,13 @@ void CTestConcur::Test_Set()
 				}
 				npc = 0;
 			}
-			//m_mutex.unlock();
+			m_mutex.unlock();
 			//m_bExit = true;
 		}
-		m_redis.DetachConnection(m_redis.HASH_SLOT(ssKey.str()), connection);
-		std::this_thread::sleep_for(std::chrono::milliseconds(1));
+		//m_redis.DetachConnection(m_redis.HASH_SLOT(ssKey.str()), connection);
+		std::this_thread::sleep_for(std::chrono::milliseconds(10));
 		if (nIndex > 10)
 			nIndex = 1;
-		m_mutex.unlock();
+		//m_mutex.unlock();
 	}
 }
